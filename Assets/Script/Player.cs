8265a2ccf38961767c81a1e6e15c9f42a4d24b04
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-17 01:05:57 +0700

    player left right

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] public float speed;
+    public float Xrange = 4;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +15,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // leaw sai leaw kwa
+        var moveAction = InputSystem.actions.FindAction("Move");
+        var hInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(hInput * speed * Time.deltaTime * Vector3.right);
+
+
+        if (transform.position.x < -Xrange)
+        {
+            transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > Xrange)
+        {
+            transform.position = new Vector3(4, transform.position.y, transform.position.z);
+        }
+
+
+        // plyer go straight
         Transform t = GetComponent<Transform>();
         t.Translate(0,0,speed * Time.deltaTime);
     }
