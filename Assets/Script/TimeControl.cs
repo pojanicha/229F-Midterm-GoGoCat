783798a128f7eb513e523c6f270d8603e95b7af9
pojanicha <pojanicha.sora@bumail.net>
2026-03-18 20:57:21 +0700

    CoutDown (Done)

---
+++ Assets/Script/TimeControl.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+
+public class TimeControl : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] float timeScale; // Put the number in the Unity Inspector (Put as Second ex. 2 mins == 120 sec)
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeScale > 0)
+        {
+            timeScale -= Time.deltaTime;
+        }
+
+        else
+        {
+            timeScale = 0;
+        }
+
+        int minutes = Mathf.FloorToInt(timeScale / 60);
+        int seconds = Mathf.FloorToInt(timeScale % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+
+    }
+}
