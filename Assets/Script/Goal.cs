84767a71eeb1c893db59ad7b28af3c1663bc5135
pojanicha <pojanicha.sora@bumail.net>
2026-03-19 15:08:29 +0700

    Win (Done)

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneController.Instance.LoadScene("Credit");
+        }
+    }
+
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
