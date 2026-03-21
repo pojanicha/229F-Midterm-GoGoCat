f8894e6aed1c4e5831cad8f60eef1b7abda7079f
Whitewolf5557 <onnicha.piti@bumail.net>
2026-03-21 20:51:17 +0700

    Ui coin

--- Assets/Script/CoinCollection.cs
+++ Assets/Script/CoinCollection.cs
@@ -5,13 +5,14 @@ public class CoinCollection : MonoBehaviour
 {
 	private int Coin = 0;
 
-	public TextMeshProUGUI coinText;
+	public TextMeshProUGUI CoinText;
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.CompareTag("Coin"))
+		if (other.CompareTag("Coin")) //Check if the collide object has tag "Coin"
 		{
 			Coin++;
+			CoinText.text = "Coin: " + Coin.ToString();
 			Debug.Log(Coin);
 			Destroy(other.gameObject);
 		}
