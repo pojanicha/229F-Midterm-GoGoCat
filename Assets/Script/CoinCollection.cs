971847c2f206b05cff3a34ecafbb38fb38552415
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-22 14:25:26 +0700

    Fix add time

--- Assets/Script/CoinCollection.cs
+++ Assets/Script/CoinCollection.cs
@@ -5,7 +5,6 @@ public class CoinCollection : MonoBehaviour
 {
 	private int Coin = 0;
 	private int maxCoin = 10;
-	private int currentCoin;
 	private int addTime = 5;
 
 	public TextMeshProUGUI CoinText;
@@ -15,15 +14,15 @@ public class CoinCollection : MonoBehaviour
 		if (other.CompareTag("Coin")) //Check if the collide object has tag "Coin"
 		{
 			Coin++;
-			currentCoin++;
+			
 			CoinText.text = "Coin: " + Coin.ToString();
 			Debug.Log(Coin);
 			Destroy(other.gameObject);
 
-			if (currentCoin == maxCoin)
+			if (Coin == maxCoin)
 			{
 				TimeControl.Instance.AddTime(addTime);
-				currentCoin = 0;
+				Coin = 0;
 			}
 		}
 	}
